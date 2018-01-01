44c0893d1849241f14f466093db6ee50b6e71df8
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2017-12-31 21:15:52 -0300

    main: compile example with crow and vireo

--- main.cpp
+++ main.cpp
@@ -1,30 +1,24 @@
 #include "vireo/base_h.h"
 #include "vireo/base_cpp.h"
-
 #include "vireo/demux/movie.h"
-//#include "vireo/decode/video.h"
-//#include "vireo/encode/h264.h"
-//#include "vireo/mux/mp4.h"
-//#include "vireo/util/util.h"
-//
+#include "vireo/mux/mp4.h"
+#include "vireo/util/util.h"
+
 #include "crow_all.h"
 
-int main()
-{
-    crow::SimpleApp app;
+int main() {
+	crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/")([](){
-        return "Hello w0rldzz!";
-    });
+	CROW_ROUTE(app, "/")([](){
+		return "Hello w0rldzz!";
+	});
 
-    app.port(8080).multithreaded().run();
+	app.port(8080).multithreaded().run();
 }
 
-void transcode(std::string in, std::string out) {
+void remux(string in, string out) {
   vireo::demux::Movie movie(in);
-//  vireo::decode::Video decoder(movie.video_track);
-//  vireo::encode::H264 encoder(decoder, 30.0f, 3, movie.video_track.fps());
-//  vireo::mux::MP4 muxer(encoder);
-//  auto binary = muxer();
-//  vireo::util::save(out, binary);
+  vireo::mux::MP4 muxer(movie.video_track);
+  auto binary = muxer();
+  vireo::util::save(out, binary);
 }
