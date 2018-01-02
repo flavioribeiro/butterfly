504ef60def29e4d74f5f924153ba96257159a8d8
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2018-01-02 18:36:20 -0300

    main: create VideoChunk struct and improve request validation

--- main.cpp
+++ main.cpp
@@ -1,29 +1,34 @@
+#include <iostream>
+
 #include "vireo/base_h.h"
 #include "vireo/base_cpp.h"
 #include "vireo/demux/movie.h"
 
 #include "crow_all.h"
 
-bool is_request_valid(const crow::request& req) {
-  return req.url_params.get("bitrate") != nullptr;
-}
-
-struct Media {
+struct VideoChunk {
   string filename;
-  long bitrate;
-  short width;
-  short height;
-  long timestamp_init;
-  long timestamp_end;
+  long   bitrate;
+  short  width;
+  short  height;
+};
+
+bool is_request_valid(const crow::request& req) {
+  std::cout << "valitading request" << std::endl;
+  return req.url_params.get("bitrate") != nullptr &&
+    req.url_params.get("width") != nullptr && 
+    req.url_params.get("height") != nullptr;
 }
 
-std::ostringstream transcode(Media chunk) {
+std::ostringstream transcode(VideoChunk chunk) {
   std::ostringstream transcoded_chunk;
 
-  vireo::demux::Movie movie(filename);
-  // check https://github.com/twitter/vireo/blob/master/vireo/tools/chunk/main.cpp#L88
-  vireo::mux::MP4 muxer(movie.video_track);
-  auto binary = muxer();
+//  vireo::demux::Movie movie(filename);
+//  // check https://github.com/twitter/vireo/blob/master/vireo/tools/chunk/main.cpp#L88
+//  vireo::mux::MP4 muxer(movie.video_track);
+//  auto binary = muxer();
+// 
+  transcoded_chunk << "ok" << chunk.bitrate;
 
   return transcoded_chunk;
 }
@@ -32,24 +37,23 @@ int main() {
   crow::SimpleApp app;
 
   CROW_ROUTE(app, "/<string>")([](const crow::request& req, string filename) {
-    std::ostringstream os;
+    VideoChunk chunk;
 
     if (!is_request_valid(req)) {
       return crow::response(400);
     }
 
     try {
-      long bitrate = boost::lexical_cast<long>(req.url_params.get("bitrate"));
+      chunk = {
+        filename,
+        boost::lexical_cast<long>(req.url_params.get("bitrate")),
+        boost::lexical_cast<short>(req.url_params.get("width")),
+      };
     } catch (const boost::bad_lexical_cast &) {
       return crow::response(400);
     }
 
-    chunk = Media{
-      filename: filename,
-      bitrate: bitrate
-    }
-
-    os = transcode(chunk);
+    std::ostringstream os = transcode(chunk);
     return crow::response{os.str()};
   });
 
