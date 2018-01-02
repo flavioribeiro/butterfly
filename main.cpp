a716587fd972f2fcdd23e82714326ef0e741168b
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2018-01-02 00:22:54 -0300

    main: prototyping how to call the transcode func

--- main.cpp
+++ main.cpp
@@ -1,24 +1,58 @@
 #include "vireo/base_h.h"
 #include "vireo/base_cpp.h"
 #include "vireo/demux/movie.h"
-#include "vireo/mux/mp4.h"
-#include "vireo/util/util.h"
 
 #include "crow_all.h"
+
+bool is_request_valid(const crow::request& req) {
+  return req.url_params.get("bitrate") != nullptr;
+}
+
+struct Media {
+  string filename;
+  long bitrate;
+  short width;
+  short height;
+  long timestamp_init;
+  long timestamp_end;
+}
+
+std::ostringstream transcode(Media chunk) {
+  std::ostringstream transcoded_chunk;
+
+  vireo::demux::Movie movie(filename);
+  // check https://github.com/twitter/vireo/blob/master/vireo/tools/chunk/main.cpp#L88
+  vireo::mux::MP4 muxer(movie.video_track);
+  auto binary = muxer();
+
+  return transcoded_chunk;
+}
 
 int main() {
   crow::SimpleApp app;
 
-  CROW_ROUTE(app, "/")([](){
-    return "Hello w0rldzz!";
+  CROW_ROUTE(app, "/<string>")([](const crow::request& req, string filename) {
+    std::ostringstream os;
+
+    if (!is_request_valid(req)) {
+      return crow::response(400);
+    }
+
+    try {
+      long bitrate = boost::lexical_cast<long>(req.url_params.get("bitrate"));
+    } catch (const boost::bad_lexical_cast &) {
+      return crow::response(400);
+    }
+
+    chunk = Media{
+      filename: filename,
+      bitrate: bitrate
+    }
+
+    os = transcode(chunk);
+    return crow::response{os.str()};
   });
 
+  app.loglevel(crow::LogLevel::Debug);
   app.port(8080).multithreaded().run();
-}
-
-void remux(string in, string out) {
-  vireo::demux::Movie movie(in);
-  vireo::mux::MP4 muxer(movie.video_track);
-  auto binary = muxer();
-  vireo::util::save(out, binary);
-}
+} 
